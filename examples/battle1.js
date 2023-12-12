ba9e9af44c34e0d380aed830de098ed92ad6c52a
Dave Berzack <daveberzack@gmail.com>
2023-12-12 10:47:48 -0500

    updated battle testing

--- examples/battle1.js
+++ examples/battle1.js
@@ -1,6 +1,8 @@
 export const hero = {
     health: 100,
     takeDamage: function(amount){
-        this.health -= amount;
+        if (amount>0) {
+            this.health -= amount;
+        }
     }
 }
--- examples/battle1.test.js
+++ examples/battle1.test.js
@@ -1,12 +1,24 @@
 import { hero } from './battle1.js';
 
+
 describe('battle1 > takeDamage', () => {
-  it("takes damage and updates health correctly", () => {
+
+  beforeEach(() => {
     hero.health = 100;
-    hero.takeDamage(10);
-    expect(hero.health).toBe(90);
-    hero.takeDamage(20);
-    expect(hero.health).toBe(70);
   });
 
+  it("takes damage and updates health correctly", () => {
+    hero.takeDamage(10);
+    expect(hero.health).toBe(90);
+  });
+
+  it("doesn't change health with zero damage", () => {
+    hero.takeDamage(0);
+    expect(hero.health).toBe(100);
+  });
+
+  it("doesn't take negative damage", () => {
+    hero.takeDamage(-20);
+    expect(hero.health).toBe(100);
+  });
 });
