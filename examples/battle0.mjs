3a34436c507df01c0b3ec5bb4c65cea50bc532f9
Dave Berzack <daveberzack@gmail.com>
2023-12-23 23:07:32 -0500

    revisions

--- examples/battle0.mjs
+++ examples/battle0.mjs
@@ -13,4 +13,4 @@ console.log("health 1:"+hero.health);
 hero.takeDamage(0);
 console.log("health 2:"+hero.health);
 hero.takeDamage(-20);
-console.log("health 2:"+hero.health);
+console.log("health 3:"+hero.health);
